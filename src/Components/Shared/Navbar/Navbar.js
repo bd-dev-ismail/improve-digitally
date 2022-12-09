3b15083f5e5d5692991f01416f91ce56e2cde2c4
bd-dev-ismail <ismailratul7@gmail.com>
2022-12-09 16:00:54 +0600

    responsiv navbar

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -33,8 +33,11 @@ const Navbar = () => {
      );
     return (
       <div>
-        <div class="px-4 py-6 mx-auto sm:max-w-xl md:max-w-full  md:px-24 lg:px-24" style={{ boxShadow: '0px 0px 10px 0px rgb(0 0 0 / 50%)'}}>
-          <div class="relative flex items-center justify-between">
+        <div
+          class="px-4 py-4 lg:py-6 mx-auto sm:max-w-xl md:max-w-full  md:px-24 lg:px-24"
+          style={{ boxShadow: "0px 0px 10px 0px rgb(0 0 0 / 50%)" }}
+        >
+          <div class="relative flex flex-row-reverse lg:flex-row items-center justify-between">
             <div class="flex items-center">
               <a
                 href="/"
@@ -45,9 +48,28 @@ const Navbar = () => {
                 <img
                   src={logo}
                   alt=""
+                  className="w-28 h-8 lg:w-64 lg:h-14"
                   style={{ maxWidth: "85%", marginTop: "-10px" }}
                 />
               </a>
+              <ul class="flex items-center lg:hidden ">
+                <li>
+                  <a
+                    href="/"
+                    class="inline-flex font-bold items-center text-secondary justify-center h-12  "
+                  >
+                    <BiPhoneCall className="w-10 mr-2 h-9 bg-primary rounded-3xl py-2 hover:text-secondary text-white" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    class="font-bold  text-white btn-sm border-0 hover:bg-primary  capitalize btn btn-secondary  rounded-3xl"
+                  >
+                    Get a Proposal
+                  </a>
+                </li>
+              </ul>
               <ul class="flex items-center justify-center hidden space-x-8 lg:flex">
                 {menuItem}
               </ul>
@@ -58,7 +80,7 @@ const Navbar = () => {
                   href="/"
                   class="inline-flex font-bold items-center text-secondary justify-center h-12 px-6 "
                 >
-                  <BiPhoneCall className='w-10 mr-2 h-9 bg-primary rounded-3xl py-2 hover:text-secondary text-white' /> 
+                  <BiPhoneCall className="w-10 mr-2 h-9 bg-primary rounded-3xl py-2 hover:text-secondary text-white" />
                   +91 98712 32887
                 </a>
               </li>
@@ -75,10 +97,10 @@ const Navbar = () => {
               <button
                 aria-label="Open Menu"
                 title="Open Menu"
-                class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                class="p-2 -mr-1 text-secodnary font-bold"
                 onClick={() => setIsMenuOpen(true)}
               >
-                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                <svg class="w-6 text-secondary" viewBox="0 0 24 24">
                   <path
                     fill="currentColor"
                     d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -104,34 +126,17 @@ const Navbar = () => {
                           title="Company"
                           class="inline-flex items-center"
                         >
-                          <svg
-                            class="w-8 text-deep-purple-accent-400"
-                            viewBox="0 0 24 24"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeMiterlimit="10"
-                            stroke="currentColor"
-                            fill="none"
-                          >
-                            <rect x="3" y="1" width="7" height="12" />
-                            <rect x="3" y="17" width="7" height="6" />
-                            <rect x="14" y="1" width="7" height="6" />
-                            <rect x="14" y="11" width="7" height="12" />
-                          </svg>
-                          <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                            Company
-                          </span>
+                          <img src={logo} alt="" />
                         </a>
                       </div>
                       <div>
                         <button
                           aria-label="Close Menu"
                           title="Close Menu"
-                          class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                          class="p-2 -mt-2 -mr-2 text-primary"
                           onClick={() => setIsMenuOpen(false)}
                         >
-                          <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <svg class="w-7 text-primary" viewBox="0 0 24 24">
                             <path
                               fill="currentColor"
                               d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -142,62 +147,22 @@ const Navbar = () => {
                     </div>
                     <nav>
                       <ul class="space-y-4">
+                        {menuItem}
                         <li>
                           <a
                             href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            class="inline-flex font-bold items-center text-secondary justify-center h-12 "
                           >
-                            Product
+                            <BiPhoneCall className="w-10 mr-2 h-9 bg-primary rounded-3xl py-2 hover:text-secondary text-white" />
+                            +91 98712 32887
                           </a>
                         </li>
                         <li>
                           <a
                             href="/"
-                            aria-label="Our product"
-                            title="Our product"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                            class="font-bold text-white border-0 hover:bg-primary  capitalize btn btn-secondary px-8 rounded-3xl"
                           >
-                            Features
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="Product pricing"
-                            title="Product pricing"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Pricing
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            aria-label="About us"
-                            title="About us"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            About us
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Sign in
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            href="/"
-                            class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                            aria-label="Sign up"
-                            title="Sign up"
-                          >
-                            Sign up
+                            Get a Proposal
                           </a>
                         </li>
                       </ul>
